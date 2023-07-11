ad0b64ce0b2c3e7b0584ae6c3aeb143a7f631863
Chiranthangowda25 <98877340+Chiranthangowda25@users.noreply.github.com>
2023-07-11 21:00:05 +0530

    Add files via upload

---
+++ python basic.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print
+'hello world'
+
+
+# In[2]:
+
+
+Print ("hello world")
+
+
+# In[3]:
+
+
+print(hello world)
+
+
+# In[4]:
+
+
+print(10)
+
+
+# In[5]:
+
+
+print(10.7)
+
+
+# ####xdggfhdjkhdj
+
+# ######sfgdsfhdhfmmm
+
+# In[6]:
+
+
+name="chiranthan"
+print ("enter name is",name)
+
+
+# ####GFIDBDOSB
+
+# In[ ]:
+
+
+
+
